879ee15de2d2bb58a872e2fcf4ec183724b5bbd8
montefr <francesco.montelli1997@gmail.com>
2017-08-07 15:39:15 +0200

    comment

--- script.sh
+++ script.sh
@@ -2,21 +2,20 @@
 
 day0=$(date '+%Y%m%d') 
 day1=$(date --date='1 day ago' '+%Y%m%d' )
-src=/root/src/
-dst=/root/dst/$day0
-lnk=/root/dst/$day1
+src=/root/src/ #the folder that has to be bcked
+dst=/root/dst/$day0 #destination folder w/ today date
+lnk=/root/dst/$day1 #incremental folder w/ yesterdate date
 optInc='-avh --link-dest="$lnk"'
 optFul='-avh'
-if [ -d "$lnk" ]; #la cartella della data di ieri esiste
+if [ -d "$lnk" ]; 
 then
-	#se la folder esiste eseguo
-	mkdir $dst #creo la folder di oggi
+	
+	mkdir $dst 
 	eval rsync $optInc $src $dst
 else
-	#non esitono versioni precedenti
-	mkdir $dst #creo la folder di oggi
-	eval rsync $optFul $src $dst #eseguo completo
+	
+	mkdir $dst 
+	eval rsync $optFul $src $dst
 fi
 
-#eval rsync $opt $src $dst
 
