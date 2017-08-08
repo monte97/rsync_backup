00c7ff9db5e19b04067853c657b450280e5fcac8
montefr <francesco.montelli1997@gmail.com>
2017-08-08 09:59:03 +0200

    now the backup is based on the most recent folder

--- script.sh
+++ script.sh
@@ -1,19 +1,22 @@
 #! /bin/bash
 
+#dst must contaion ONLY the backups, can be on a remote destination. see rsynk documentation
 day0=$(date '+%Y%m%d') 
-day1=$(date --date='1 day ago' '+%Y%m%d' )
 src=/root/src/ #the folder that has to be bcked
-dst=/root/dst/$day0 #destination folder w/ today date
-lnk=/root/dst/$day1 #incremental folder w/ yesterdate date
+dst_root=/root/dst #the destination folder root
+dst=$dst_root/$day0 #destination folder w/ today date
+last=$(ls $dst_root -Art | tail -n 1) 
+lnk=/root/dst/$last #we synk w/ the most recent folder
 optInc='-avh --link-dest="$lnk"'
 optFul='-avh'
-if [ -d "$lnk" ]; 
+if [ -n "$last"  ]; 
 then
-	
+	#full backup
 	mkdir $dst 
 	eval rsync $optInc $src $dst
 else
 	
+	#incremental backup
 	mkdir $dst 
 	eval rsync $optFul $src $dst
 fi
