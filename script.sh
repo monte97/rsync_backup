9e61d8611b780df68f6b68625e97e0d07729bf92
montefr <francesco.montelli1997@gmail.com>
2017-08-09 15:19:44 +0200

    added removal of old folder if above a threshold

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+
 #dst must contaion ONLY the backups, can be on a remote destination. see rsynk documentation
 day0=$(date '+%Y%m%d') 
 src=/root/src/ #the folder that has to be bcked
@@ -9,6 +10,19 @@ last=$(ls $dst_root -Art | tail -n 1)
 lnk=/root/dst/$last #we synk w/ the most recent folder
 optInc='-avh --link-dest="$lnk"'
 optFul='-avh'
+
+
+#while we are above the occupation threshoold we keep deleting the oldest folder in the backup root
+disksize=$(du -sk $dst_root |cut -f1)
+disk=$(df -k $dir | nawk '!Filesystem/ print{$2})')
+percentage=eval($disksize/$disk)
+limit=85
+
+while [ $percentage -gt $limit]
+do
+	rm -r $(ls $dst_root -t -r | head -n 1)
+done 
+
 if [ -n "$last"  ]; 
 then
 	#full backup
