2b75d091135ec576443115083b453c522ca7a148
monte97 <monte97@users.noreply.github.com>
2017-08-27 10:09:18 +0200

    Update script.sh
    first stable version

--- script.sh
+++ script.sh
@@ -3,35 +3,36 @@
 
 #dst must contaion ONLY the backups, can be on a remote destination. see rsynk documentation
 current_day=$(date '+%Y%m%d') 
-src=/root/src/ #the folder that has to be bcked
-dst_root=/root/dst #the destination folder root
-dst=$dst_root/current_day #destination folder w/ today date 
-lnk=/root/dst/$(ls $dst_root -Art | tail -n 1) #we synk w/ the most recent folder
+src=/root/src/ #the folder that has to be bcked, contains the subfolder
+dst_root=/srv/dev-disk-by-label-pool/dst
+dst=$dst_root/$current_day #destination folder w/ today date 
+last=$(ls $dst_root -Art | tail -n 1) 
+lnk=$dst_root/$last
 optInc='-avh --delete --link-dest="$lnk"'
 optFul='-avh'
-limit=85
 
-#chande sda w/ the device wich contains the backup folder
-percentage=$(df | grep 'sda' | awk 'BEGIN{} {percent+=$5;} END{print percent}')
+echo $lnk
+
+
+percentage=$(df | grep 'md0' | awk 'BEGIN{} {percent+=$5;} END{print percent}')
 while [ $percentage -gt 85 ]
 do
+	echo "rimozione"
 	rm -r $(ls $dst_root -t -r | head -n 1)
-	percentage=$(df | grep 'sda' | awk 'BEGIN{} {percent+=$5;} END{print percent}')
+	percentage=$(df | grep 'md0' | awk 'BEGIN{} {percent+=$5;} END{print percent}')
 done 
 
-
-
-
-if [ -n "$last"  ]; 
+touch output.log && echo "" > output.log
+touch error.log && echo "" > error.log
+if [ -d "$lnk"  ]; 
 then
-	#full backup
 	mkdir $dst 
-	eval rsync $optInc $src $dst
-else
-	
-	#incremental backup
-	mkdir $dst 
-	eval rsync $optFul $src $dst
+	eval rsync $optInc $src $dst > output.log 2> error.log ||  echo "" | mutt -s "rsync  error" -a output.log error.log -- backup @edilweb.net 
+		
+else				
+	mkdir $dst 	
+	eval rsync $optFul $src $dst > output.log 2> error.log || echo "" |  mutt -s "rsync  error" -a output.log error.log --  backup@edilweb.net 
 fi
 
-
+touch diskOccupation.txt && echo "" > diskOccupation.txt && df > diskOccupation.txt
+cat diskOccupation.txt | mutt -s "Summary" -a diskOccupation.txt output.log -- backup@edilweb.net
